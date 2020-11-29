f0d5636bf17838868b37e434d2e74b283ef73cca
Vadim Colcev <vadimkolchev@gmail.com>
2020-11-29 14:05:46 +0200

    Initial Release

---
+++ MemeMaker/CaptionChoice.swift
@@ -0,0 +1,14 @@
+//
+//  CaptionChoice.swift
+//  MemeMaker
+//
+//  Created by Vadim Colcev on 11/29/20.
+//
+
+import Foundation
+
+struct CaptionOption: Equatable {
+    var emoji: String
+    var caption: String
+}
+
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let topChoices = [CaptionOption(emoji: "👀", caption: "You know what's cool?"), CaptionOption(emoji: "💥", caption: "You know what makes me mad?"), CaptionOption(emoji: "😻", caption: "You know what I love?")]
+    let bottomChoices = [CaptionOption(emoji: "😺", caption: "Cats wearing hats"), CaptionOption(emoji: "🐶", caption: "Dogs carrying logs"), CaptionOption(emoji: "🙉", caption: "Monkeys being funky")]
+    
 
+    @IBOutlet weak var topCaptionSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var bottomCaptionSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        topCaptionSegmentedControl.removeAllSegments()
+        for choice in topChoices {
+            topCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+        }
+        topCaptionSegmentedControl.selectedSegmentIndex = 0
+        bottomCaptionSegmentedControl.removeAllSegments()
+        for choice in bottomChoices {
+            bottomCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: bottomChoices.count, animated: false)
+        }
+        bottomCaptionSegmentedControl.selectedSegmentIndex = 0
+        chooseCaption(for: topChoices)
+        chooseCaption(for: bottomChoices)
     }
-
+    
+    @IBAction func segmentedControlChanged(_ sender: Any) {
+        chooseCaption(for: topChoices)
+        chooseCaption(for: bottomChoices)
+    }
+    
+    func chooseCaption(for selectedSegment: [CaptionOption]) {
+        if selectedSegment == topChoices {
+            topCaptionLabel.text = topChoices[topCaptionSegmentedControl.selectedSegmentIndex].caption
+        } else {
+            bottomCaptionLabel.text = bottomChoices[bottomCaptionSegmentedControl.selectedSegmentIndex].caption
+        }
+    }
+    
+    
 
 }
 
